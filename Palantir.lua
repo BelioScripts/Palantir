7753d38308c318f3b610ab3dc3cbc156c2de9a7c
Belio <94620750+BelioScripts@users.noreply.github.com>
2025-12-25 20:39:05 +0000

    Update Palantir.lua

--- Palantir.lua
+++ Palantir.lua
@@ -1,3 +1,4 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BelioScripts/Palantir/refs/heads/main/Bypass.lua"))()
 --// =========================
 --// SERVICES
 --// =========================
